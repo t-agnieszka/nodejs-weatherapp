e625fed274f259dc770a025e7947b5289524c8cd
agnieszka.tatarzyn <agnieszka.tatarzyn@rewardgateway.com>
2020-04-18 13:08:39 +0100

    weather description

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -3,18 +3,18 @@ const request = require('request');
 const forecast = (lat, long, callback) => {
     const url = 'https://api.darksky.net/forecast/a147450e403f23f5ad93646457a6a249/' + encodeURIComponent(lat) + ',' + encodeURIComponent(long) + '?units=si';
 
-    request({url, json: true}, (error, {body}) => {
+    request({ url, json: true }, (error, { body }) => {
 
-    if (error) {
-        callback('Unable to connect to weather service', undefined);
-    } else if (body.error) {
-        callback('Unable to find a location', undefined);   
-    } else {
-        const {temperature, precipProbability} = body.currently;
-        callback(undefined, 'It is currently ' + temperature + ' degrees out. There is a ' + precipProbability + '% chance of rain.');
-    }
-});
+        if (error) {
+            callback('Unable to connect to weather service', undefined);
+        } else if (body.error) {
+            callback('Unable to find a location', undefined);
+        } else {
+            const { temperature, precipProbability } = body.currently;
+            callback(undefined, 'It is currently ' + temperature + ' degrees out. The high today is ' + body.daily.data[0].temperatureHigh + ' with a low of ' + body.daily.data[0].temperatureLow + '. There is a ' + precipProbability + '% chance of rain.');
+        }
+    });
 }
- module.exports = forecast;
- 
+module.exports = forecast;
+
 
